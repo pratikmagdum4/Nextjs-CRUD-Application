26ab22114c2d85b92f3f26def6f36e14543f9198
pratik <pratiksunilmagdum2021@gmail.com>
2024-12-06 19:08:58 +0530

    minor

--- app/api/topics/[id]/route.js
+++ app/api/topics/[id]/route.js
@@ -3,18 +3,18 @@ import Topic from "@/models/topic";
 import { NextResponse } from "next/server";
 
 
-export async function PUT(request, { params }) {
-
-    const { id } = params;
+export async function PUT(request, context) {
+    const { id } = context.params; // Correct way to access params
     const { newTitle: title, newDescription: description } = await request.json();
     await connectMongoDB();
     await Topic.findByIdAndUpdate(id, { title, description });
     return NextResponse.json({ message: "Topic updated" }, { status: 200 });
 }
-export async function GET(request, { params }) {
-    const { id } = params;
-    await connectMongoDB();
 
-    const topic = await Topic.findOne({ _id: id });
+
+export async function GET(request, context) {
+    const { id } = context.params; // Correct way to access params
+    await connectMongoDB();
+    const topic = await Topic.findById(id);
     return NextResponse.json({ topic }, { status: 200 });
 }
--- components/TopicsList.jsx
+++ components/TopicsList.jsx
@@ -6,7 +6,6 @@ const getTopics = async () => {
     try {
         const res = await fetch('http://localhost:3000/api/topics', {
             cache: 'no-store',
-
         })
         if (!res.ok) {
             throw new Error("Failed to fetch the data");
@@ -18,10 +17,12 @@ const getTopics = async () => {
 }
 export default async function TopicsList() {
     const { topics } = await getTopics();
+    const gotTopics = topics.topics;
+    console.log("The topics are ", gotTopics)
     return (
         <>
             {topics.map(t => (
-                <div className="p-4 border border-slate-300 my-3 flex justify-between gap-5 items-start">
+                <div key={t._id} className="p-4 border border-slate-300 my-3 flex justify-between gap-5 items-start">
                     <div>
                         <h2 className="font-bold text-2xl">
                             {t.title}
